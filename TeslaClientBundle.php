32f8ac16495d6b88f13d9ff9d44dad83c2cef3f4
24HOURSMEDIA <info@24hoursmedia.com>
2013-06-19 23:35:25 +0200

    NEW - # 2705: set up bundle https://web.24hoursmedia.com/mantis/view.php?id=2705

--- TeslaClientBundle.php
+++ TeslaClientBundle.php
@@ -1,22 +1,19 @@
 <?php
-
 namespace Tesla\Bundle\ClientBundle;
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Tesla\Bundle\ClientBundle\DependencyInjection\Compiler\ConfigurationCheckPass;
 
-
 /**
- *
- *
+ * Bundle
  * @author eapbachman
- *
  */
 class TeslaClientBundle extends Bundle
 {
-	public function build(ContainerBuilder $container)
+
+	public function build (ContainerBuilder $container)
 	{
+		// check configuration
 		$container->addCompilerPass(new ConfigurationCheckPass());
 	}
 }
